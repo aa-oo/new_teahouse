a9983742a45ad4f23b93bfc72a83c3df96f2516f
hreeng <shl635306159@163.com>
2021-07-21 17:01:22 +0800

    7/21

--- pages/me/me-buscard.vue
+++ pages/me/me-buscard.vue
@@ -1,13 +1,27 @@
-<!-- <template>
+<template>
 	<view class="">
-	
+		<view class="status_bar"> <!-- 这里是状态栏 --> </view>
 		<uni-nav-bar left-icon="back" left-text="返回" right-text="菜单" title="导航栏组件"></uni-nav-bar>
+		
 	</view>
 </template>
 <script>
-import uniNavBar from '@/components/uni-nav-bar/uni-nav-bar.vue'
-export default {
-    components: {uniNavBar}
-}
-</script> -->
+	import uniNavBar from '@/uni_modules/uni-nav-bar/uni-nav-bar.vue'
+	export default {
+		data(){
+			return{
+				
+			}
+		},
+		components: {
+			// uniNavBar,
+			}
+	}
+</script>
+<style>
+	.status_bar {
+	      height: var(--status-bar-height);
+	      width: 100%;
+	  }
+</style>
 
